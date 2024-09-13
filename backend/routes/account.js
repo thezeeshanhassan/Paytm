ab804d02dc5ef0d855cc9737de203689c5469d9d
thezeeshanhassan <thezeeshanhassan@gmail.com>
2024-09-13 12:01:23 +0500

    Transections Added

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -3,6 +3,8 @@ const { route } = require("./user");
 const authMiddleware = require("../middleware");
 const Account = require("../Models/account");
 const { findOneAndUpdate, updateOne } = require("../Models/user");
+const { mongo, default: mongoose } = require("mongoose");
+
 
 const router = express.Router();
 
@@ -17,24 +19,30 @@ router.get("/balance", authMiddleware, async (req, res) => {
 });
 
 router.post("/transfer", authMiddleware, async (req, res) => {
-  const { to, amount } = req.body;
+  //Creating Session
+  const session = await mongoose.startSession();
+  session.startTransaction();
 
+  const { to, amount } = req.body;
+  // Find Recevier Account
   const toAccount = await Account.findOne({
     userId: to,
-  });
-
+  }).session(session);
+  // Find Sender Account
   const fromAccount = await Account.findOne({
     userId: req.userId,
-  });
+  }).session(session);
 
   if (fromAccount.balance < amount) {
+    await session.abortTransaction();
     return res.status(400).json({
       message: "Insufficient Balance",
-      user : fromAccount
+      user: fromAccount,
     });
   }
 
   if (!toAccount) {
+    await session.abortTransaction();
     return res.status(400).json({
       message: "Account Does Not Exists",
     });
@@ -45,15 +53,16 @@ router.post("/transfer", authMiddleware, async (req, res) => {
       userId: req.userId,
     },
     { $inc: { balance: -amount } }
-  );
+  ).session(session);
 
   await Account.updateOne(
     {
       userId: to,
     },
     { $inc: { balance: amount } }
-  );
+  ).session(session);
 
+  await session.commitTransetion();
   return res.status(200).json({
     message: "Amount Successfully Sent",
   });
